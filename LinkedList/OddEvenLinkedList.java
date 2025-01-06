1c1e9ea39b1b186e6af373b3f33cca252a016826
ARYESH <143291958+Aryesh10@users.noreply.github.com>
2025-01-06 22:18:18 +0530

    Add files via upload

---
+++ LinkedList/OddEvenLinkedList.java
@@ -0,0 +1,59 @@
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class OddEvenLinkedList {
+
+    public static ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        head = oddEvenList(head);
+
+        System.out.println("Reordered list:");
+        printList(head);
+    }
+}
